e837dba274f143a5f99300a3f496df6d54b55f6f
ali pashaei <apsgbm@gmail.com>
2019-05-10 07:50:10 +0430

    frist commit

---
+++ konex/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+   <appnav></appnav>
+    
+    <router-view class="container"/>
+  </div>
+</template>
+<script>
+import appnav from '@/components/appnav';
+export default {
+  components:{
+    appnav,
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ konex/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+   <div class="jumbotron" style="text-align:right;">
+    <h1 class="display-3"> اطمینان و اعتبار قطعهKONEX </h1>
+    <p class="lead">مفتخریم که قطعاتی باکیفیت برای هموطنان خود ساخته ایم</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+  
+};
+</script>
